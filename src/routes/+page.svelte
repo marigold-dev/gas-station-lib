0a91a5f29c5e730f12f5902266e8e3decca76ae8
Arthur Guillon <arthur@marigold.dev>
2023-09-04 22:18:15 +0200

    Environment vars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { myAccount, getPKH, Tezos } from "$lib/tezos";
+  import { PUBLIC_PERMIT } from '$env/static/public';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>Adresse du contrat = { PUBLIC_PERMIT }</p>
+
