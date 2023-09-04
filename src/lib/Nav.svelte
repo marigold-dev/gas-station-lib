ba49d06d59b5399e9a2e8cc3e566c5ff659bc7d0
Arthur Guillon <arthur@marigold.dev>
2023-09-04 22:16:22 +0200

    PicoCSS and Navbar

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { myAccount, connectWallet } from '$lib/tezos';
+</script>
+
+<header class="header">
+  <nav>
+    <ul>
+      <!-- TODO: icon? name? -->
+      <li><a href="/">Index</a></li>
+    </ul>
+    <ul>
+      <li>
+      <button on:click={connectWallet}>
+        Connect wallet
+      </button>
+      </li>
+    </ul>
+  </nav>
+</header>
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import "@picocss/pico";
+  import Nav from '$lib/Nav.svelte';
+</script>
+<Nav />
+<slot></slot>
